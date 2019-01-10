d2557b0738dc7b1962d44b58eedc11b40dad61c4
Montblanc159 <guillaume.zaugg@gmail.com>
2019-01-10 14:41:10 +0100

    bonjour bob - 00-hello.rb

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -2,5 +2,16 @@ def say_hello (first_name)
     puts "Bonjour #{first_name}"
 end
 
+def ask_first_name
+  puts "Quel est ton joli petit prénom ?"
+  print "> "
+  first_name = gets.chomp
+  return first_name
+end
 
-puts say_hello ("Bob")
+def perform
+  first_name = ask_first_name
+  say_hello (first_name)
+end
+
+perform
