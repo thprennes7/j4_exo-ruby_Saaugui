80f320033610cad57f92e0cf04e231c8ce2798a4
aurelien <duquenneaurelien@gmail.com>
2019-01-10 16:16:51 +0100

    full pyramid done

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,27 +1,28 @@
-def half_pyramid 
+def ask_num 
 
-puts "Salut, bienvenue dans ma super pyramide! Combien d'étage veux-tu entre 1 et 25?"
-print ">"
-user_num_floor = gets.chomp.to_i
+    puts "Salut, bienvenue dans ma super pyramide! Combien d'étage veux-tu entre 1 et 25?"
+    print "> "
+    user_num_floor = gets.chomp.to_i
     return user_num_floor
 end
 
 
-def perform (user_num_floor = 0, i = 1, j = 0)
-    
-    user_num_floor = half_pyramid
+def full_pyramid (user_num_floor = 0, i = 1, j = 0)
     
 while i < user_num_floor
-    user_num_floor.times {
+      user_num_floor.times do
         print " " * (user_num_floor - i) 
         puts "#" * (i + j)
-        i+=1
-        j +=1
-        }
+        i += 1
+        j += 1
+      end
+    end
 end
     
+def perform
+    user_num_floor = ask_num
+    full_pyramid(user_num_floor)
 end
-    perform 
-    
 
+perform
     
